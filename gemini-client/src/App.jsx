fc20edfc447dd96e9711d54f6e6309b7d69b0106
Aaron <aarondsouza42@gmail.com>
2025-01-13 20:53:30 -0800

    Site working, Basic UI and front/backend communication

--- gemini-client/src/App.jsx
+++ gemini-client/src/App.jsx
@@ -1,22 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import hampii from '../coquette-hampter.jpg';
 
 
 function App() {
 
+  const [serverData, setServerData]= useState('')
+  const [userPrompt, setUserPrompt] = useState('')
+
+
+  function handleSubmit(){
+      
+    fetch('/api', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'prompt': userPrompt})
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setServerData(data)
+        console.log(data)
+      })
+
+  }
+  
   return (
-    <main style={{height: '85vh', display: "flex", flexDirection: 'column'}}>
-      <h1 style={{paddingBottom: '10px', marginBottom: '0'}}>My Prompter</h1>
-      <div style={{margin: '0', flexGrow: '1'}}>
+    <main style={{height: '100vh', display: "flex", flexDirection: 'column'}}>
+      <h1 style={{ color: '#fff7ef', paddingRight:'10px', paddingBottom: '10px', marginBottom: '0'}}> Boss GPT
+            <img src={hampii} style={{width:55, height:40}} alt="Hampster"/>
+
+      </h1>
+
+      <text style={{color: '#AA336A'}}> Be your on GPT</text>
+      <div style={{margin: '0', flexGrow: '1', overflow:'scroll'}}>
         <div style= {{width: '100%', height: '100%'}}>
-          <article style= {{margin: '0'}}/>
+          <article style= {{margin: '0'}}>
+              {serverData}
+            </article>
+        </div>
         </div>
 
-        <div style= {{display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px'}}>
-            <textarea style= {{margin: '10', flexGrow: '1', overflowY: 'hidden'}}  placeholder='Type Prompt..'/>
-            <button style= {{margin: '100', flexGrow: '1', padding: '15px'}} >GO!</button>
+        <div style= {{display: 'flex', alignItems: 'end', background: 'pink', padding: '10px'}}>
+            <textarea onChange={(e) => setUserPrompt(e.target.value)} style= {{margin: '0', flexGrow: '1', overflowY: 'hidden'}}  placeholder='Type Prompt..'/>
+            <button onClick={handleSubmit} style= {{background: '#F9629F', margin: '10', padding: '20px'}} >GO!</button>
 
         </div>
-      </div>
 
     </main>  
 
--- palm-server/index.js
+++ palm-server/index.js
@@ -1,27 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
-
+const bodyParser = require('body-parser');
+const { TextServiceClient } = require('@google-ai/generativelanguage').v1beta2;
+app.use(bodyParser.json());
+  
+const { GoogleAuth } = require("google-auth-library");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const API_KEY = process.env.API_KEY
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const MODEL = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
 
-  const prompt = "whats after one, two";
 
-app.get('/api', async (req, res) => {
 
-  try{
-    const result = await model.generateContent(prompt);
-    res.json(result.response.text())
-  }
-  catch(error)
-  {
-    app.send("Error processing request from API")
-  }
-       
+
+app.post('/api', async (req, res) => {
+   prompt = req.body.prompt;
+   
+    try{
+        console.log(prompt);
+        result = await MODEL.generateContent(prompt);
+        answer = result.response.candidates[0].content.parts[0].text;
+        console.log(answer);
+        res.json(answer)
+      }
+    catch(error){
+      console.log(error);
+    }
+
 });
 
 app.listen(3333, () => console.log('Server running'));

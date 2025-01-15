e4f3b87d8919aa4a567454b6d248951a74835505
Aaron <aarondsouza42@gmail.com>
2025-01-15 03:20:59 -0800

    Finished basic request/response UI, minor UI changes, loading screen

--- gemini-client/src/App.jsx
+++ gemini-client/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import hampii from '../coquette-hampter.jpg';
+import {marked} from 'marked'
+import DOMPurify from 'dompurify';
 
+marked.use({
+  gfm: true,
+
+})
 
 function App() {
 
   const [serverData, setServerData]= useState('')
   const [userPrompt, setUserPrompt] = useState('')
+  const inputRef = useRef(null)
+
+
+  function handleKeyDown(e){
+    if(e.key == 'Enter' && !e.shiftKey){
+      e.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+
+    }
+  }
 
 
   function handleSubmit(){
-      
+    setServerData('')
+
+    if(userPrompt !== ''){
     fetch('/api', {
         method: 'POST',
         headers:{
@@ -19,9 +38,14 @@ function App() {
     })
       .then((res) => res.json())
       .then((data) => {
-        setServerData(data)
-        console.log(data)
+        const htmll = DOMPurify.sanitize(marked(data))
+        setServerData(htmll)
+        inputRef.current.focus()
+        setUserPrompt('')
       })
+      .catch((error) => 
+        console.error("Error fetching data:", error));
+    }
 
   }
   
@@ -35,14 +59,18 @@ function App() {
       <text style={{color: '#AA336A'}}> Be your on GPT</text>
       <div style={{margin: '0', flexGrow: '1', overflow:'scroll'}}>
         <div style= {{width: '100%', height: '100%'}}>
-          <article style= {{margin: '0'}}>
-              {serverData}
-            </article>
+
+          {
+            (serverData === '') ? ('....Loading'):
+              <article style= {{margin: '0'}} dangerouslySetInnerHTML={{__html: serverData}}/>
+          }
         </div>
+
         </div>
 
         <div style= {{display: 'flex', alignItems: 'end', background: 'pink', padding: '10px'}}>
-            <textarea onChange={(e) => setUserPrompt(e.target.value)} style= {{margin: '0', flexGrow: '1', overflowY: 'hidden'}}  placeholder='Type Prompt..'/>
+            <textarea onChange={(e) => setUserPrompt(e.target.value)} 
+            onKeyDown={handleKeyDown} ref={inputRef} value={userPrompt} style= {{margin: '0', flexGrow: '1', overflowY: 'hidden'}}  placeholder='Type Prompt..'/>
             <button onClick={handleSubmit} style= {{background: '#F9629F', margin: '10', padding: '20px'}} >GO!</button>
 
         </div>
